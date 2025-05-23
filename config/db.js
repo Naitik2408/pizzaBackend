990f13af6249493a2fcf5a9c9fc78b924ddade2c
naitik <naitik2408>
2025-05-23 21:26:07 +0530

    degug issue

--- config/db.js
+++ config/db.js
@@ -6,6 +6,7 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  // console.log("database uri: ",process.env.MONGO_URI);
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
